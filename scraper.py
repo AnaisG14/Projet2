9ddb312c0a87f06b24ce388e03759aaa2f19add9
AnaisG14 <gatard.anais@gmail.com>
2021-08-28 10:21:46 +0200

    ajouts lignes de création des fichiers FichiersCSV et Images

--- scraper.py
+++ scraper.py
@@ -1,15 +1,25 @@
 # -*-coding:utf-8-*-
+
+# ============================== imports ==============================
+
 import csv
 import requests as rq
 from bs4 import BeautifulSoup
+import os
+
+# ============================= fonctions =============================
 
 def create_soup(url):
     """ Pick up html code in url and return a soup object"""
     page_content = rq.get(url, auth=('user', 'pass'))
     return BeautifulSoup(page_content.content, 'html.parser')
 
+
 def get_information_book(url_book):
-    """ Pick up information from url_book and extract information."""
+    """ Pick up information from url_book and extract information: title,
+    upc, prices, review rating, number available, product description,
+    image url and category.
+    Save all informations in a dictionnary and return this dictionnary"""
     soup = create_soup(url_book)
 
     # pick up all information of the web site
@@ -28,46 +38,59 @@ def get_information_book(url_book):
     price_including_tax = float(price_including_tax[1:])
     review_rating = int(table_contenus[6])
     number_available_text = table_contenus[5]
-    number_available = int(number_available_text.replace("In stock (","").replace(" available)",""))
+    number_available = int(number_available_text.replace("In stock (", "").replace(" available)", ""))
     # pick up product description
     balises_p = soup.find_all("p")
     product_description = (balises_p[3]).string
     # pick up image url and category
     balises_a = soup.find_all("a")
     category = (balises_a[3]).string
-    image_url = (soup.img).get('src')
+    image_url = soup.img.get('src')
     image_url = (image_url.replace("../..", "http://books.toscrape.com"))
 
     # save all information in a dictionnary
-    information_product = {}
-    information_product["product_page_url"] = url_book
-    information_product["upc"] = upc
-    information_product["title"] = title
-    information_product["price_including_tax"] = price_including_tax
-    information_product["price_excluding_tax"] = price_excluding_tax
-    information_product["number_available"] = number_available
-    information_product['product_description'] = product_description
-    information_product['category'] = category
-    information_product['review_rating'] = review_rating
-    information_product["image_url"] = image_url
+    information_product = {"product_page_url": url_book,
+                           "upc": upc, "title": title,
+                           "price_including_tax": price_including_tax,
+                           "price_excluding_tax": price_excluding_tax,
+                           "number_available": number_available,
+                           "product_description": product_description,
+                           "category": category,
+                           "review_rating": review_rating,
+                           "image_url": image_url
+                           }
     download_image(image_url, title_to_save)
     print(category)
     return information_product
 
+
 def create_file_csv(filename):
+    """ Create a file named filenamed with only the header line """
+    # create file FichierCSV if it does not exist
+    if os.path.isdir('FichiersCSV'):
+        pass
+    else:
+        os.mkdir("FichiersCSV")
+
     with open(filename, 'w') as csvfile:
-        fieldnames = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax',
-                      'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+        fieldnames = ['product_page_url', 'upc', 'title',
+                      'price_including_tax', 'price_excluding_tax',
+                      'number_available', 'product_description',
+                      'category', 'review_rating', 'image_url']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
+
 
 def save_file_csv(filename, dicttosave):
     """ Save in filename.csv a dictionnary named dicttosave"""
     with open(filename, 'a') as csvfile:
-        fieldnames = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax',
-                      'number_available', 'product_description', 'category', 'review_rating', 'image_url']
+        fieldnames = ['product_page_url', 'upc', 'title',
+                      'price_including_tax', 'price_excluding_tax',
+                      'number_available', 'product_description',
+                      'category', 'review_rating', 'image_url']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writerow(dicttosave)
+
 
 def get_all_pages(url_category):
     """ Return a list of all pages for one category"""
@@ -75,13 +98,14 @@ def get_all_pages(url_category):
     link_page = url_category
     while link_page:
         soup = create_soup(link_page)
-        next = soup.select_one(".next > a")
-        if next:
-            link_page = f'{url_category.replace("index.html","")}{next.get("href")}'
+        next_page = soup.select_one(".next > a")
+        if next_page:
+            link_page = f'{url_category.replace("index.html","")}{next_page.get("href")}'
             list_url_in_one_category.append(link_page)
         else:
             link_page = ""
     return list_url_in_one_category
+
 
 def get_url_book(url_category):
     """ Return a list of url of all the books for url_category"""
@@ -101,6 +125,7 @@ def get_url_book(url_category):
             # list_balises_parent.append(balise_parent)
     return list_url_book
 
+
 def get_url_category():
     """ Return a list of all url category of the site http://books.toscrape.com/"""
     soup = create_soup('http://books.toscrape.com/')
@@ -114,24 +139,36 @@ def get_url_category():
     del categories_url_name[0]
     return categories_url_name
 
+
 def sup_caractere_special(text):
-    """retourne le texte sans les caractères spéciaux : .:/\n et espaces afin de les utiliser
-    comme nom de fichier"""
+    """retourne le texte sans les caractères spéciaux : .:/\n et espaces
+    afin de les utiliser comme nom de fichier.
+    """
     caracteres_speciaux = [".", "/", ":", "\n", " "]
     for car in caracteres_speciaux:
         if car in text:
             text = text.replace(car, "")
     return text
 
+
 def download_image(url, title):
     """ Download an image and save it as its title book"""
+    # create file Images if it does not exist
+    if os.path.isdir("Images"):
+        pass
+    else:
+        os.mkdir("Images")
+    # save image
     url_image = (rq.get(url)).content
     with open(f"Images/{title}", 'wb') as f:
         f.write(url_image)
     print(f"Image downloaded as {title}")
 
+
 def save_informations_all_books(categories_url_name):
-    """ For each url_categoru, save all information book in one file for one category"""
+    """ For each url_categoru, save all information book in
+    one file for one category
+    """
     # return list of url for each book of the category
     categories_url_name = categories_url_name
     for item in categories_url_name:
@@ -152,16 +189,18 @@ def scraper():
     """ Scrapping of the site "https://books.toscrape.com :
         - get all url of categories of the site,
         - get all url of all books for each category
-        - scrap each book and save in file.csv : product_page_url, upc, title, price_including_tax,
-        price_excluding tax, number available, product_description, category, review_rating,
-        image_url
+        - scrap each book and save in file.csv : product_page_url, upc,
+        title, price_including_tax,price_excluding tax, number available,
+        product_description, category, review_rating, image_url
         - download image for each book.
-        At the end of the scrapping, you must have 2 new directories named "Fichiers.csv" and "Images"
-        contend one file.csv for each category and all images.
+        At the end of the scrapping, you must have 2 new directories named
+        "Fichiers.csv" and "Images" contend one file.csv for each category
+        and all images.
         """
     categories_url_name = get_url_category()
     save_informations_all_books(categories_url_name)
 
-################### main project ######################################################
+# ============================ main project ============================
+
 if __name__ == '__main__':
     scraper()
