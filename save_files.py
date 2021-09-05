1c757c7ff1368b85be7f450a5778e84369bc43d5
AnaisG14 <gatard.anais@gmail.com>
2021-09-05 09:20:50 +0200

    Résolution de problème d'encodage sous windows, ajout de caractères spéciaux et spécifacation à l'écriture de l'encodage

--- save_files.py
+++ save_files.py
@@ -3,7 +3,7 @@ import csv
 
 
 def create_file_csv(filename):
-    with open(filename, 'w') as csvfile:
+    with open(filename, 'w', encoding="utf-8") as csvfile:
         fieldnames = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax',
                       'number_available', 'product_description', 'category', 'review_rating', 'image_url']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
@@ -12,7 +12,7 @@ def create_file_csv(filename):
 
 def save_file_csv(filename, dicttosave):
     """ Save in filename.csv a dictionnary named dicttosave"""
-    with open(filename, 'a') as csvfile:
+    with open(filename, 'a', encoding="utf-8") as csvfile:
         fieldnames = ['product_page_url', 'upc', 'title', 'price_including_tax', 'price_excluding_tax',
                       'number_available', 'product_description', 'category', 'review_rating', 'image_url']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
--- utils.py
+++ utils.py
@@ -12,7 +12,7 @@ def create_soup(url):
 def sup_caractere_special(text):
     """retourne le texte sans les caractères spéciaux : .:/\n et espaces afin de les utiliser
     comme nom de fichier"""
-    caracteres_speciaux = [".", "/", ":", "\n", " "]
+    caracteres_speciaux = [".", "/", ":", "\n", " ", "'", "\"", "*", "?"]
     for car in caracteres_speciaux:
         if car in text:
             text = text.replace(car, "")
